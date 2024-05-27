85e825e66087cc2f2cfb17664221fce2a1285405
Bashdi <31628993+bashdi@users.noreply.github.com>
2024-05-27 22:03:59 +0200

    Refactor

---
+++ cmdImplementation.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type CmdLineRepresentation struct {
+}
+
+func (clr *CmdLineRepresentation) DrawBoard(board [][]int) {
+	for i := range board {
+		for _, field := range board[i] {
+			if field == 0 {
+				fmt.Printf(" #  ")
+			} else {
+				fmt.Printf(" # %d", field)
+			}
+
+		}
+		fmt.Printf(" #\n")
+	}
+}
+
+func (clr *CmdLineRepresentation) AnnouncePlayersTurn(s string) {
+	fmt.Printf("%s ist dran\n", s)
+}
+
+func (clr *CmdLineRepresentation) AnnounceWinner(s string) {
+	fmt.Printf("%s hat gewonnen!\n", s)
+}
+
+func (clr *CmdLineRepresentation) AnnounceDraw() {
+	fmt.Println("Unentschieden! Es konnte kein Gewinner ermittelt werden!")
+}
+
+type VgPlayerCmdLine struct {
+	name string
+}
+
+func NewVgPlayerConsoleInput(name string) *VgPlayerCmdLine {
+	return &VgPlayerCmdLine{name: name}
+}
+
+func (vgpc *VgPlayerCmdLine) GetPlayerName() string {
+	return vgpc.name
+}
+
+func (vgpc *VgPlayerCmdLine) DoTurn(board [][]int) int {
+	fmt.Println("Enter column:")
+	var column int
+	_, err := fmt.Scanf("%d", &column)
+	if err != nil {
+		return -1
+	}
+	return column
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	viergewinnt "4gewinnt/vierGewinnt"
-	"fmt"
 )
 
 func main() {
@@ -14,55 +13,4 @@ func main() {
 	vg.AddPlayer(viergewinnt.NewVgPlayerComputer("Computer4"))
 
 	vg.StartGame()
-}
-
-type CmdLineRepresentation struct {
-}
-
-func (clr *CmdLineRepresentation) DrawBoard(board [][]int) {
-	for i := range board {
-		for _, field := range board[i] {
-			if field == 0 {
-				fmt.Printf(" #  ")
-			} else {
-				fmt.Printf(" # %d", field)
-			}
-
-		}
-		fmt.Printf(" #\n")
-	}
-}
-
-func (clr *CmdLineRepresentation) AnnouncePlayersTurn(s string) {
-	fmt.Printf("%s ist dran\n", s)
-}
-
-func (clr *CmdLineRepresentation) AnnounceWinner(s string) {
-	fmt.Printf("%s hat gewonnen!\n", s)
-}
-
-func (clr *CmdLineRepresentation) AnnounceDraw() {
-	fmt.Println("Unentschieden! Es konnte kein Gewinner ermittelt werden!")
-}
-
-type VgPlayerCmdLine struct {
-	name string
-}
-
-func NewVgPlayerConsoleInput(name string) *VgPlayerCmdLine {
-	return &VgPlayerCmdLine{name: name}
-}
-
-func (vgpc *VgPlayerCmdLine) GetPlayerName() string {
-	return vgpc.name
-}
-
-func (vgpc *VgPlayerCmdLine) DoTurn(board [][]int) int {
-	fmt.Println("Enter column:")
-	var column int
-	_, err := fmt.Scanf("%d", &column)
-	if err != nil {
-		return -1
-	}
-	return column
 }
